48427b696c4cdefafdb35409cb14da0da8d71032
VongolaWu <vongola8757@gmail.com>
2024-01-03 22:00:33 +0800

    app.js update cors

--- server/app.js
+++ server/app.js
@@ -1,15 +1,13 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const PORT = 3000;
 
+app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('QR Code Website is running!');
-});
-
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
 });
 
 const db = require('./database');
@@ -17,5 +15,6 @@ const qrRoutes = require('./routes/qr');
 
 app.use('/api/qr', qrRoutes);
 
-const cors = require('cors');
-app.use(cors());
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
